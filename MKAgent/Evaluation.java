c45b926b9d69b1d8ac105fba0b49e98e5072482b
Xiaotan Zhu <xiaotan.zhu@student.manchester.ac.uk>
2020-12-12 00:46:51 +0800

    Heuristic

--- MKAgent/Evaluation.java
+++ MKAgent/Evaluation.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Evaluation {
-    public static int getValue(Board board, Side mySide) {
-        int value = getSeedDiff(board, Side.SOUTH) + maxMoveScore(board, Side.SOUTH);
-        if (mySide == Side.SOUTH)
+    public static int getValue(Board board, Side playerSide) {
+        int value = getSeedDiff(board, playerSide) + maxMoveScore(board, playerSide);
+        if (Main.mySide == playerSide)
             return value;
         return -value;
     }
@@ -17,16 +17,20 @@ public class Evaluation {
 
     private static ArrayList<Move> getMovesEndAtPos(Board board, Side playerSide, int des) {
         ArrayList<Move> moves = new ArrayList<>();
+        int oneRound = 2 * board.getNoOfHoles() + 1;
         for (int i = 1; i <= board.getNoOfHoles(); i++) {
-            if (board.getSeeds(playerSide, i) == des - i)
+            int seedNum = board.getSeeds(playerSide, i);
+            if (seedNum > 0 && seedNum % oneRound == des - i)
                 moves.add(new Move(playerSide, i));
         }
         return moves;
     }
 
     private static boolean canMoveEndAtPos(Board board, Side playerSide, int des) {
+        int oneRound = 2 * board.getNoOfHoles() + 1;
         for (int i = 1; i <= board.getNoOfHoles(); i++) {
-            if (board.getSeeds(playerSide, i) == des - i)
+            int seedNum = board.getSeeds(playerSide, i);
+            if (seedNum > 0 && seedNum % oneRound == des - i)
                 return true;
         }
         return false;
@@ -36,30 +40,20 @@ public class Evaluation {
         return getMovesEndAtPos(board, playerSide, board.getNoOfHoles()+1);
     }
 
-    private static int getMaxCaptureScore(Board board, Side playerSide, Move move) {
+    private static int getMaxCaptureScore(Board board, Side playerSide) {
         int maxNum = 0;
         for(int i = 1; i <= board.getNoOfHoles(); ++i) {
             if (board.getSeeds(playerSide, i) == 0 && canMoveEndAtPos(board, playerSide, i)) {
-                if (board.getSeedsOp(playerSide, i) > maxNum) {
-                    move.setSide(playerSide);
-                    move.setHole(i);
-                    maxNum = board.getSeedsOp(playerSide, i);
-                }
+                if (board.getSeedsOp(playerSide, i) > maxNum)
+                    maxNum = board.getSeedsOp(playerSide, i) + 1;
             }
         }
-        // opp seeds captured and our seed
-        return maxNum + 1;
+        return maxNum;
     }
 
     private static int maxMoveScore(Board board, Side playerSide) {
         ArrayList<Integer> scores = new ArrayList<>();
-        Move simulationMove = new Move(Side.SOUTH, 1);
-        int score = getMaxCaptureScore(board, playerSide, simulationMove);
-        if (playerSide == Main.mySide) {
-            Board simulationBoard = new Board(board);
-            Kalah.makeMove(simulationBoard, simulationMove);
-            score -= getMaxCaptureScore(simulationBoard, playerSide.opposite(), simulationMove);
-        }
+        int score = getMaxCaptureScore(board, playerSide);
         scores.add(score);
         for (Move move : getMoveAgainMoves(board, playerSide)) {
             Board simulationBoard = new Board(board);
